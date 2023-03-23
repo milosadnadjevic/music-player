d59fb9686f8d6ae9b2e40179757fe3cf9d9801dc
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-03-23 13:36:45 -0400

    play buttons works

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,15 @@
+import {useRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faAngleLeft, faAngleRight, faPlay} from "@fortawesome/free-solid-svg-icons"
 
-const Player = () => {
+const Player = ( {currentSong}) => {
+
+    const audioRef = useRef(null)
+
+    const playSongHandler = () => {
+        audioRef.current.play()
+
+    }
     return (
         <div className="player">
                     <div className="time-control">
@@ -11,9 +19,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x"/>
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
             </div>
+            <audio src={currentSong.audio} ref={audioRef} />
         </div>
     )
 }
