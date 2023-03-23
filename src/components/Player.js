1d9c3b1782e2b975c92a0906744b6801f94297c4
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-03-23 11:06:17 -0400

    styles and icons added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faAngleLeft, faAngleRight, faPlay} from "@fortawesome/free-solid-svg-icons"
+
 const Player = () => {
     return (
         <div className="player">
-            <h1>Player</h1>
-            <div className="time-control">
+                    <div className="time-control">
                 <p>start time</p>
                 <input type='range'></input>
                 <p>end time</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x"/>
+                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
             </div>
         </div>
     )
