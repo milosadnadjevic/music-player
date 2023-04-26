e0ba8fe4a3eb183b25beec5c28e7c8a565c75da9
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-04-26 03:11:23 +0200

    LibrarySong and Library added

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,23 @@
 import LibrarySong from "./LibrarySong"
 
 
-const Library = ({songs}) => {
+const Library = ({songs, setCurrentSong}) => {
     return (
         <div className="library">
             <h2>Libray</h2>
             <div className="library-songs">
-                {songs.map(song => <LibrarySong song={song}/>)}
+                {songs.map((song) => (
+                <LibrarySong
+                songs={songs}
+                setCurrentSong={setCurrentSong}
+                song={song}
+                id={song.id}
+                key={song.id}
+                audioRef={audioRef}
+                isPlaying={isPlaying}
+                setSongs={setSongs}
+                />
+               ))}
 
             </div>
         </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,11 +1,18 @@
 
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({song, songs, setCurrentSong, id}) => {
+    const songSelectHandler = () => {
+        const selectedSong = songs.filter((state) => state.id === id)
+        setCurrentSong(selectedSong[0])
+    }
     return (
-        <div className="library-song">
+        <div onClick={songSelectHandler} className="library-song">
             <img src={song.cover} alt={song.name} />
+            <div className="song-description">
             <h2>{song.name}</h2>
             <h3>{song.artist}</h3>
+            </div>
+            
         </div>
     )
 }
